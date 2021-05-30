414988a5b06aaad8ae12b67fcfcc445097e60ac4
Javi Gomez <rtomyj@gmail.com>
2021-05-30 12:15:46 -0500

    Cleaned up Status route

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,15 +1,15 @@
-import status from './status'
+import Status from './Status'
 import {Express} from 'express'
 
 export default class Routes
 {
 
 	static BASE_URI = '/heart/api/v1'
-	static API_STATUS_URI = `${Routes.BASE_URI}/testcall`
+	static API_STATUS_URI = `${Routes.BASE_URI}/status`
 
 	static setupRoutes = (app: Express) =>
 	{
-		app.use(Routes.API_STATUS_URI, status)
+		app.use(Routes.API_STATUS_URI, Status.router)
 	}
 
 }
