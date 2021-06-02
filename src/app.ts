4268a481ebd23075df21d91807d3d384ba20ab9e
Javi Gomez <rtomyj@gmail.com>
2021-06-02 18:27:49 -0500

    Opened up CORS for requests from SKC-Site

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import HttpConfig from './http/HttpConfig'
 import Routes from './routes/Routes'
 import RequestErrorHandling from './http/RequestErrorHandling'
 
 const app = express()
+
+/*
+	handles pre-flight
+	server will return the allowed CORS functionality and stop processing the rest of the request, ie prevents error due to redirects in pre-flight
+	Should be one of the first middleware added.
+*/
+app.options( '*', cors() )
+app.use( cors() )	// opens up all CORS settings to clients
 
 HttpConfig.setupHttpConnection(app)
 
