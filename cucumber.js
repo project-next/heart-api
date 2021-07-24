3ce342074dccb5f13ee0dfd15a088a2e712b048b
Javi Gomez <rtomyj@gmail.com>
2021-07-24 10:54:36 -0500

    Updated feature/code for cucumber test to call/test service

--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,7 @@ let common = [
 	'integration-test',                // Specify our feature files
 	'--require-module ts-node/register',    // Load TypeScript module
 	'--require **/step-definitions/**/*.ts',   // Load step definitions
+	'--publish-quiet',
  ].join(' ');
 
  module.exports = {
