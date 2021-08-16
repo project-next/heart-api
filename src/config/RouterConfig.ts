af4190335b2cf35ef0a5f2cfc0ffe6e88bf4e30c
Javi Gomez <rtomyj@gmail.com>
2021-08-16 17:06:18 -0500

    Split controller logic from router logic

--- src/config/RouterConfig.ts
+++ src/config/RouterConfig.ts
@@ -1,15 +1,15 @@
 import Status from '../router/Status'
-import YouTubeChannelActivity from '../router/YouTubeChannelActivity'
 import { Express } from 'express'
 import ytVideoInfoRouter from '../router/YouTubeVideoInfo'
 import ytGiveawayRouter from '../router/YouTubeGiveaway'
+import ytChannelActivityRouter from '../router/YouTubeChannelActivity'
 
 export default class Routes {
 	static BASE_URI = '/v1'
 
 	static setupRoutes(app: Express): void {
 		app.use(Routes.BASE_URI, new Status().router)
-		app.use(Routes.BASE_URI, new YouTubeChannelActivity().router)
+		app.use(Routes.BASE_URI, ytChannelActivityRouter)
 		app.use(Routes.BASE_URI, ytVideoInfoRouter)
 		app.use(Routes.BASE_URI, ytGiveawayRouter)
 	}
---
+++ src/controller/YouTubeChannelActivityController.ts
@@ -0,0 +1,131 @@
+
+import { Request, Response } from 'express'
+import { AxiosError, AxiosResponse } from 'axios'
+import Constants from '../constants/Constants'
+import YouTubeAxiosConfig from '../service/YouTubeAxiosConfig'
+import moize from 'moize'
+import HeartAPIError from '../error/HeartAPIError'
+import YouTubeUploadsResponse, { FormattedUploadResponse } from '../model/YouTubeUploadsResponse'
+
+type YouTubeAPIResponse = {
+	kind: string
+	etag: string
+	id: string
+	snippet: {
+		publishedAt: string;
+		channelId: string,
+		title: string,
+		description: string,
+		thumbnails: {
+			default: {
+				url: string
+				width: string
+				height: string
+			}
+			medium: {
+				url: string
+				width: string
+				height: string
+			}
+			high: {
+				url: string
+				width: string
+				height: string
+			}
+			standard: {
+				url: string
+				width: string
+				height: string
+			}
+			maxres: {
+				url: string
+				width: string
+				height: string
+			}
+		}
+		channelTitle: string
+		type: string
+	}
+	contentDetails: {
+		upload: {
+			videoId: string
+		}
+	}
+}
+
+
+/**
+ * Exposes an endpoint that clients can use to get information about YouTube Video Uploads.
+ * There will be checks to prevent unwanted users from using this API to prevent Quota Limit errors.
+ * YouTube API output is cleaned up and only the most useful info is returned to client.
+ * @param router object that will be used to expose functionality.
+*/
+export default function YouTubeChannelActivityController() {
+	return (req: Request, res: Response) => {
+		if (req.query.channelId === undefined || req.query.channelId === null) {
+			const status = 422
+
+			res.status(status)
+			res.json(new HeartAPIError('Empty or null channelId.', status))
+
+			res.end()
+			return
+		}
+
+		let channelId = req.query.channelId.toString()
+		if (!Constants.VALID_YOUTUBE_CHANNEL_IDS.includes(channelId))	// prevent malicious use of API
+		{
+			const status = 400
+
+			res.status(status)
+			res.json(new HeartAPIError('This API cannot use provided channelId. Only certain Id\'s are permitted.', status))
+
+			res.end()
+			return
+		}
+
+
+		memoizedYouTubeRequest(channelId)
+			.then((ytResponse: AxiosResponse) => {
+				const videoIds: string[] = []
+
+				const formattedYtResponse: FormattedUploadResponse[] = ytResponse.data.items.map((youTubeVidInfo: YouTubeAPIResponse): FormattedUploadResponse | void => {
+					if (youTubeVidInfo.snippet.type === 'upload') {
+						const videoId = youTubeVidInfo.contentDetails.upload.videoId.toString()
+						videoIds.push(videoId)
+
+						return {
+							id: videoId
+							, title: youTubeVidInfo.snippet.title
+							, description: youTubeVidInfo.snippet.description
+							, publishedAt: youTubeVidInfo.snippet.publishedAt
+							, thumbnailUrl: youTubeVidInfo.snippet.thumbnails.high.url
+							, url: `https://www.youtube.com/watch?v=${videoId}`
+						}
+					}
+				})
+
+
+				res.status(200)
+				res.json(new YouTubeUploadsResponse(formattedYtResponse, formattedYtResponse.length))
+				res.end()
+			})
+			.catch((error: AxiosError) => YouTubeAxiosConfig.youtubeAPIErrorCallback(error, res))
+	}
+}
+
+
+
+/**
+ * Function definition that uses memoization with expiration policy to prevent exceeding quota limits Google uses.
+*/
+const memoizedYouTubeRequest = moize((channelId: string) => {
+	return YouTubeAxiosConfig.YOUTUBE_UPLOADS_AXIOS_BASE_CONFIG
+		.get('/activities'	// documentation for endpoint -> https://developers.google.com/youtube/v3/docs/activities/list
+			, {
+				params: {
+					channelId: channelId
+				}
+			}
+		)
+}, { maxAge: 1000 * 60 * 15, updateExpire: false })
