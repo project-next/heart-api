303ec64bc2ab5d025061712fd08e876719c41f90
Javi Gomez <rtomyj@gmail.com>
2021-08-18 17:45:32 -0500

    Updates as per updates to DBs and collections

--- src/mongo/Connection.ts
+++ src/mongo/Connection.ts
@@ -1,19 +1,21 @@
 import { connect, connection } from 'mongoose'
 import fs from 'fs'
 
+const DB_NAME = 'services'
+
 
 export default function mongoDBConn() {
 	const pem = fs.readFileSync('./certs/mongoDB-heart-api-X509.pem')
 
 	connection.on('error', () => {
-		console.log('There was an error connecting to heart-api:news mongoDB')
+		console.log(`There was an error connecting to heart-api:${DB_NAME} mongoDB`)
 	})
 
 	connection.once('open', () => {
-		console.log('Connection successfully established with heart-api:news mongoDB')
+		console.log(`Connection successfully established with heart-api:${DB_NAME} mongoDB`)
 	})
 
-	connect('mongodb+srv://heart-api-cluster.g0vnw.mongodb.net/news', {
+	connect(`mongodb+srv://heart-api-cluster.g0vnw.mongodb.net/${DB_NAME}`, {
 		useNewUrlParser: true,
 		useFindAndModify: false,
 		useCreateIndex: true,
--- src/mongo/models/NewsModel.ts
+++ src/mongo/models/NewsModel.ts
@@ -1,18 +1,18 @@
 import { Schema, Document, model } from 'mongoose'
 
-export type skc = Document & {
+export type news = Document & {
 	title: string,
 	body: string,
 	tags: string[]
 }
 
 
-const News_SKCSchema: Schema = new Schema({
+const NewsSchema: Schema = new Schema({
 	title: { type: String, required: false, unique: false },
 	body: { type: String, required: false, unique: false },
 	tags: [{ type: String, required: false, unique: false }]
 })
 
 
-const News_SKCModel = model<skc>('skc', News_SKCSchema, 'skc')
-export default News_SKCModel
+const NewsModel = model<news>('news', NewsSchema, 'news')
+export default NewsModel
