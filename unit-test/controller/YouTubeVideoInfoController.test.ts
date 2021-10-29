7ac5c9baa868d4c33992c99ead7aeb726b079c97
Javi <rtomyj@gmail.com>
2021-10-28 20:50:36 -0500

    Fixed unit tests. Deleted unused tests. Using supertest.

--- unit-test/controller/YouTubeVideoInfoController.test.ts
+++ unit-test/controller/YouTubeVideoInfoController.test.ts
@@ -1,93 +1,65 @@
 import { expect } from 'chai'
-
-import chai from 'chai'
-import chaiHttp from 'chai-http'
+import request from 'supertest'
 import app from '../../src/app'
 import YouTubeAxiosConfig from '@config/YouTubeAxiosConfig'
 import sinon, { SinonStub } from 'sinon'
 import { YouTubeAPIUploadsResponse } from '../../src/types/YouTubeAPIVideoTypes'
+import Constants from '@helper/Constants'
+// import * as JWTAuthentication from '@middleware/JWTAuthentication'
 
 describe('YouTubeVideoInfo tests', () => {
-	chai.use(chaiHttp)
-	let API_KEY: string | undefined
-	let stub: SinonStub
+	let youtubeAxiosStub: SinonStub
+	// let jwtStub: SinonStub
 
 	before(() => {
-		API_KEY = process.env.HEART_API_KEY
+		// jwtStub = sinon
+		// 	.stub(JWTAuthentication, 'default')
+
+		// jwtStub.returns(null)
 	})
 
 
 	beforeEach(() => {
-		stub = sinon
+		youtubeAxiosStub = sinon
 			.stub(YouTubeAxiosConfig.YOUTUBE_VIDEO_INFO_AXIOS_BASE_CONFIG, 'get')
 	})
 
 
 	afterEach(() => {
-		stub.restore()
-	})
-
-
-	it('Calling Video Info endpoint - missing key param', done => {
-		chai
-			.request(app)
-			.get('/v1/yt/video/info?videoId=okINSj2Okxw')
-			.end((err, res) => {
-				expect(res.status).to.equal(401)
-
-				expect(res.body).to.not.be.empty
-				expect(res.body.code).to.equal(401)
-				expect(res.body.description).to.equal('API key is missing')
-
-				expect(stub.notCalled).to.be.true
-				done()
-			})
+		youtubeAxiosStub.restore()
+		// jwtStub.resetHistory()
 	})
 
 
 	it('Calling Video Info endpoint - missing videoId param', done => {
-		chai
-			.request(app)
-			.get(`/v1/yt/video/info?key=${API_KEY}`)
+		request(app)
+			.get(`/api/v1/yt/video/info`)
 			.end((err, res) => {
+				console.log(res)
 				expect(res.status).to.equal(400)
 
 				expect(res.body).to.not.be.empty
 				expect(res.body.code).to.equal(400)
-				expect(res.body.description).to.equal('Missing required query params.')
+				expect(res.body.description).to.equal(Constants.MISSING_REQUIRED_PARAM_MESSAGE)
 
-				expect(stub.notCalled).to.be.true
+				expect(youtubeAxiosStub.notCalled).to.be.true
 				done()
 			})
 	})
 
 
-	it('Calling Video Info endpoint - using incorrect key value', done => {
-		chai.request(app).get('/v1/yt/video/info?videoId=okINSj2Okxw&key=XXXXXXX').end((err, res) => {
-			expect(res.status).to.equal(403)
-
-			expect(res.body).to.not.be.empty
-			expect(res.body.code).to.equal(403)
-			expect(res.body.description).to.equal('API key is incorrect')
-
-			expect(stub.notCalled).to.be.true
-			done()
-		})
-	})
-
-
 	it('Calling Video Info endpoint - YouTube API returns with non-error but body is empty', done => {
-		stub.resolves(Promise.resolve({status: 200, data: undefined}))
+		youtubeAxiosStub.resolves(Promise.resolve({status: 200, data: undefined}))
 
-		chai.request(app)
-			.get(`/v1/yt/video/info?videoId=okINSj2Okxw&key=${API_KEY}`)
+		request(app)
+			.get(`/api/v1/yt/video/info?videoId=okINSj2Okxw`)
 			.then((res) =>{
 				expect(res.status).to.equal(500)
 
 				expect(res.body).to.not.be.empty
 				expect(res.body.code).to.equal(500)
 
-				expect(stub.calledOnce).to.be.true	// since requests are memoized a call must have happened using stub or else test will use wrong response data
+				expect(youtubeAxiosStub.calledOnce).to.be.true	// since requests are memoized a call must have happened using youtubeAxiosStub or else test will use wrong response data
 				done()
 			})
 	})
@@ -103,15 +75,15 @@ describe('YouTubeVideoInfo tests', () => {
 				resultsPerPage: 0
 			}
 		} as YouTubeAPIUploadsResponse
-		stub.resolves({status: 200, data: returnData})
+		youtubeAxiosStub.resolves({status: 200, data: returnData})
 
-		chai.request(app).get(`/v1/yt/video/info?videoId=RANDOM&key=${API_KEY}`).end((err, res) => {
+		request(app).get(`/api/v1/yt/video/info?videoId=RANDOM`).end((err, res) => {
 			expect(res.status).to.equal(200)
 
 			expect(res.body).to.not.be.empty
 			expect(res.body.validVideo).to.be.false
 
-			expect(stub.calledOnce).to.be.true	// since requests are memoized a call must have happened using stub or else test will use wrong response data
+			expect(youtubeAxiosStub.calledOnce).to.be.true	// since requests are memoized a call must have happened using youtubeAxiosStub or else test will use wrong response data
 			done()
 		})
 	})
@@ -140,9 +112,9 @@ describe('YouTubeVideoInfo tests', () => {
 				resultsPerPage: 1
 			}
 		} as YouTubeAPIUploadsResponse
-		stub.resolves({status: 200, data: returnData})
+		youtubeAxiosStub.resolves({status: 200, data: returnData})
 
-		chai.request(app).get(`/v1/yt/video/info?videoId=RANDOM-2&key=${API_KEY}`).end((err, res) => {
+		request(app).get(`/api/v1/yt/video/info?videoId=RANDOM-2`).end((err, res) => {
 			expect(res.status).to.equal(200)
 
 			expect(res.body).to.not.be.empty
@@ -153,18 +125,18 @@ describe('YouTubeVideoInfo tests', () => {
 			expect(res.body.videoStats.favorites).to.equal(0)
 			expect(res.body.videoStats.numComments).to.equal(6)
 
-			expect(stub.calledOnce).to.be.true	// since requests are memoized a call must have happened using stub or else test will use wrong response data
+			expect(youtubeAxiosStub.calledOnce).to.be.true	// since requests are memoized a call must have happened using youtubeAxiosStub or else test will use wrong response data
 			done()
 		})
 	})
 
 
 	it('Calling Video Info endpoint - YouTube server error', done => {
-		stub
+		youtubeAxiosStub
 			.rejects({code: 403, response: {error: {status: 403}}})
 
-		chai.request(app)
-			.get(`/v1/yt/video/info?videoId=YOUTUBE-ERROR&key=${API_KEY}`)
+		request(app)
+			.get(`/api/v1/yt/video/info?videoId=YOUTUBE-ERROR`)
 			.end((err, res) => {
 				expect(res.status).to.equal(500)
 
@@ -172,7 +144,7 @@ describe('YouTubeVideoInfo tests', () => {
 				expect(res.body.code).to.equal(500)
 				expect(res.body.description).to.not.be.empty
 
-				expect(stub.calledOnce).to.be.true	// since requests are memoized a call must have happened using stub or else test will use wrong response data
+				expect(youtubeAxiosStub.calledOnce).to.be.true	// since requests are memoized a call must have happened using youtubeAxiosStub or else test will use wrong response data
 				done()
 		})
 	})
