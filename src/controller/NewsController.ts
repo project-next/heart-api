ec95fa66207a9a3367761586a9a89cff308bcc5c
Javi <rtomyj@gmail.com>
2021-10-31 19:33:03 -0500

    Updated endpoint to send data from DB

--- src/controller/NewsController.ts
+++ src/controller/NewsController.ts
@@ -1,16 +1,30 @@
 import { Request, Response } from 'express'
 import { addNews, getNewsWithTag } from '@mongo/dao/NewsDAO'
+import { News } from '@mongo/models/NewsModel'
 
 export const getNewsForService = async (req: Request, res: Response) => {
 	const service = req?.query?.service as string
+	let newsItems: News[]
 
-	addNews()
 	await getNewsWithTag([service])
-	res.json({"body": service})
+		.then((news: News[]) => {
+			newsItems = news.map((newsItem): News => {
+				return {
+					title: newsItem.title,
+					content:  newsItem.content
+				} as News
+			})
+		})
+	res.json(
+		{
+			"service": service,
+			"items": newsItems!
+		}
+	)
 }
 
 
 export const addNewsForService = async (req: Request, res: Response) => {
 	addNews()
-	// res.json({"body": service})
+	res.json("working")
 }
