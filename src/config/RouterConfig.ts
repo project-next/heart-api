c116340f9d5a58723dbd1acfe919f8ccf4ba4308
Javi <rtomyj@gmail.com>
2021-11-06 21:08:33 -0500

    Cleanup

--- src/config/RouterConfig.ts
+++ src/config/RouterConfig.ts
@@ -1,10 +1,11 @@
 import { Express } from 'express'
-import validateKeyCB from '@middleware/JWTAuthentication'
+import validateJWTMiddleware from '@middleware/JWTAuthentication'
 import youTubeGiveAwayControllerCB from '@controller/YouTubeGiveAwayController'
 import youTubeChannelActivityControllerCB from '@controller/YouTubeUploadsController'
 import youTubeVideoInfoControllerCB from '@controller/YouTubeVideoInfoController'
 import statusControllerCB from '@controller/StatusController'
 import { createJwtControllerCB } from '@controller/JWTController'
+import apiKeyAuthenticationMiddleware from '@middleware/APIKeyAuthentication'
 
 export default class Routes {
 	static BASE_URI = '/api/v1'
@@ -18,8 +19,8 @@ export default class Routes {
 	static setupRoutes(app: Express): void {
 		app.get(`${Routes.BASE_URI}/status`, statusControllerCB)
 		app.get(`${Routes.YT_FUNCTIONALITY_BASE_URI}/channel/uploads`, youTubeChannelActivityControllerCB)
-		app.get(`${Routes.YT_FUNCTIONALITY_BASE_URI}/video/info`, validateKeyCB, youTubeVideoInfoControllerCB)
-		app.get(`${Routes.YT_FUNCTIONALITY_BASE_URI}/video/giveaway`, validateKeyCB, youTubeGiveAwayControllerCB)
-		app.get(`${Routes.BASE_URI}/auth/jwt`, createJwtControllerCB)
+		app.get(`${Routes.YT_FUNCTIONALITY_BASE_URI}/video/info`, validateJWTMiddleware, youTubeVideoInfoControllerCB)
+		app.get(`${Routes.YT_FUNCTIONALITY_BASE_URI}/video/giveaway`, validateJWTMiddleware, youTubeGiveAwayControllerCB)
+		app.get(`${Routes.BASE_URI}/auth/jwt`, apiKeyAuthenticationMiddleware,  createJwtControllerCB)
 	}
 }
