7ca522833d648ff98c81f3b117211b6fd63a2c5c
Javi <rtomyj@gmail.com>
2021-11-06 21:42:18 -0500

    Added validation. Ensure tags include service. Ensure tags are unique

--- src/controller/NewsController.ts
+++ src/controller/NewsController.ts
@@ -3,26 +3,34 @@ import { addNews, getNewsWithTag } from '@mongo/dao/NewsDAO'
 import { News } from '@mongo/models/NewsModel'
 import HeartAPIError from '@error/HeartAPIError'
 import capitalize from 'lodash.capitalize'
+import { uniq } from 'lodash'
 
 export async function getNewsForService(req: Request, res: Response) {
 	const service = req?.query?.service as string
-	let newsItems: News[]
 
-	await getNewsWithTag([service])
-		.then((news: News[]) => {
-			newsItems = news.map((newsItem): News => {
-				return {
-					title: newsItem.title,
-					content:  newsItem.content
-				} as News
+	if (!service) {
+		res.status(422)
+		res.json(new HeartAPIError("Query param 'service' cannot be empty", 422))
+	} else {
+		let newsItems: News[]
+
+		await getNewsWithTag([service])
+			.then((news: News[]) => {
+				newsItems = news.map((newsItem): News => {
+					return {
+						title: newsItem.title,
+						content:  newsItem.content,
+						tags:  newsItem.tags
+					} as News
+				})
 			})
-		})
-	res.json(
-		{
-			"service": service,
-			"items": newsItems!
-		}
-	)
+		res.json(
+			{
+				"service": service,
+				"items": newsItems!
+			}
+		)
+	}
 }
 
 
@@ -31,11 +39,21 @@ export async function addNewsForService(req: Request, res: Response) {
 	const content: string | undefined = req?.body?.content as string
 	const tags: string[] | undefined = req?.body?.tags as string[]
 
-	if (!(title && content)) {
+	const service: string | undefined = req?.query?.service as string
+
+	if (!(title && content && service)) {
 		res.status(422)
-		res.json(new HeartAPIError("Request body needs 'title' and 'content' values", 422))
+		res.json(new HeartAPIError("Request body needs 'title' and 'content' values. Query param 'service' cannot be empty.", 422))
 	} else {
-		addNews(capitalize(title), content, tags)
+		// add a tag specifying service name if not already present
+		if (tags.indexOf(service) === -1) {
+			tags.push(service)
+		}
+
+		// only unique tags
+		const uniqTags = uniq(tags)
+
+		addNews(capitalize(title), content, uniqTags)
 			.then(isSuccess => {
 				if (isSuccess) {
 					res.json({"status": "DB updated successfully"})
