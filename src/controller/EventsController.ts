036e1a72aba464141e058d61fbb75cb63add89ee
Javi <rtomyj@gmail.com>
2021-11-14 12:40:26 -0600

    Added model/controller for event collection

---
+++ src/controller/EventsController.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+
+export function getEventsControllerCB(req: Request, res: Response, next: NextFunction) {
+	res.json(
+		{
+			yo: 'xxxxx'
+		}
+	)
+}
+
+
+export function createEventControllerCB(req: Request, res: Response, next: NextFunction) {
+	res.json(
+		{
+			yo: 'yyyyyyy'
+		}
+	)
+}
---
+++ src/mongo/models/EventModel.ts
@@ -0,0 +1,26 @@
+import { model, Schema, Document } from 'mongoose'
+
+
+export type Event = Document & {
+	name: string,
+	date: Date,
+	service: string,
+	tags: string[]
+}
+
+
+const EventSchema = new Schema(
+	{
+		name: { type: String, required: true, unique: true },
+		date: { type: Date, required: true, unique: true },
+		service: { type: String, required: true, unique: true },
+		tags: [{ type: Date, required: true, unique: true }]
+	},
+	{
+		timestamps: true
+	}
+)
+
+
+const EventModel = model<Event>('event', EventSchema)
+export default EventModel
