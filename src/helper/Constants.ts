19a605b8d0ca7614d0e01172785c7372fadb07e9
Javi <rtomyj@gmail.com>
2022-04-03 15:01:19 -0500

    Added BTSC channel

--- src/helper/Constants.ts
+++ src/helper/Constants.ts
@@ -7,8 +7,9 @@ export default class Constants {
 
 	static readonly SKC_CHANNEL_ID = 'UCBZ_1wWyLQI3SV9IgLbyiNQ'
 	static readonly RBF_CHANNEL_ID = 'UCKSWgpqi8BnNgDYh_jwYQNw'
+	static readonly BTSC_CHANNEL_ID = 'UCu0LlZ527i4NcXNhru67D1Q'
 
-	static readonly VALID_YOUTUBE_CHANNEL_IDS: string[] = [Constants.SKC_CHANNEL_ID, Constants.RBF_CHANNEL_ID]
+	static readonly VALID_YOUTUBE_CHANNEL_IDS: string[] = [Constants.SKC_CHANNEL_ID, Constants.RBF_CHANNEL_ID, Constants.BTSC_CHANNEL_ID]
 
 	static readonly MISSING_REQUIRED_PARAM_MESSAGE = 'Missing required query params'
 }
