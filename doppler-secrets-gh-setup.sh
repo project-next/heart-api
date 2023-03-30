90ee0e8e81ed010635161245fdd3ec0ab9523777
Javi <rtomyj@gmail.com>
2023-03-30 11:57:23 -0500

    Adding certs

--- doppler-secrets-gh-setup.sh
+++ doppler-secrets-gh-setup.sh
@@ -1,7 +1,7 @@
 mkdir certs
-echo "${{ steps.doppler.outputs.SSL_PRIVATE_KEY }}" > certs/private.key
-echo "${{ steps.doppler.outputs.SSL_CA_BUNDLE_CRT }}" > certs/ca_bundle.crt
-echo "${{ steps.doppler.outputs.SSL_CERTIFICATE_CRT }}" > certs/certificate.crt
-echo "${{ steps.doppler.outputs.MONGO_DB_SSL_CERT }}" > certs/mongoDB-heart-api-X509.pem
-echo "${{ steps.doppler.outputs.JWT_PRIVATE_KEY }}" > certs/jwt.key
-echo "${{ steps.doppler.outputs.JWT_PUBLIC_KEY }}" > certs/jwt.pub
+echo "$HEART_API_SSL_PRIVATE_KEY" > certs/private.key
+echo "$HEART_API_SSL_CA_BUNDLE_CRT" > certs/ca_bundle.crt
+echo "$HEART_API_SSL_CERTIFICATE_CRT" > certs/certificate.crt
+echo "$HEART_API_MONGO_DB_SSL_CERT" > certs/mongoDB-heart-api-X509.pem
+echo "$HEART_API_JWT_PRIVATE_KEY" > certs/jwt.key
+echo "$HEART_API_JWT_PUBLIC_KEY" > certs/jwt.pub
