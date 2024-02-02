d7d340328be4bbcc73d0b5bfb23a5c5b3964cb51
Javi <8519394+rtomyj@users.noreply.github.com>
2024-02-01 19:15:51 -0600

    Adding delete flag

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo "Using server $SERVER and directory $DIR_ON_SERVER to sync prod API"
 
 echo "Uploading API files"
 rsync -avz -e "ssh -i ~/.ssh/skc-server.pem" docker-compose.yml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
-rsync -avz -e "ssh -i ~/.ssh/skc-server.pem" -r dist/* "${USER}@${SERVER}:${DIR_ON_SERVER}/dist/"
+rsync -avz --delete -e "ssh -i ~/.ssh/skc-server.pem" -r dist/* "${USER}@${SERVER}:${DIR_ON_SERVER}/dist/"
 
 echo "Restaging API"
 ssh -i ~/.ssh/skc-server.pem "${USER}@${SERVER}" << EOF
