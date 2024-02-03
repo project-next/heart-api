16ddafd2e8340a236c5de0f225891394dbcfefa1
Javi <8519394+rtomyj@users.noreply.github.com>
2024-02-03 10:28:58 -0600

    Updates

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,16 @@ fi
 
 SERVER=$1
 USER="ec2-user"
-DIR_ON_SERVER="heart-api"
+DIR_ON_SERVER="api/heart-api"
 
 echo "Using server $SERVER and directory $DIR_ON_SERVER to sync prod API"
 
 echo "Uploading API files"
-rsync -avz -e "ssh -i ~/.ssh/skc-server.pem" docker-compose.yml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
-rsync -avz --delete -e "ssh -i ~/.ssh/skc-server.pem" -r dist/* "${USER}@${SERVER}:${DIR_ON_SERVER}/dist/"
+rsync -avz -e "ssh -i ~/.ssh/project-next.pem" docker-compose.yml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
+rsync -avz --delete -e "ssh -i ~/.ssh/project-next.pem" -r dist/ "${USER}@${SERVER}:${DIR_ON_SERVER}/dist/"
 
 echo "Restaging API"
-ssh -i ~/.ssh/skc-server.pem "${USER}@${SERVER}" << EOF
+ssh -i ~/.ssh/project-next.pem "${USER}@${SERVER}" << EOF
 	cd "$DIR_ON_SERVER"
 	docker-compose kill
 	docker-compose rm -f
