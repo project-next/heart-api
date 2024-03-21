1b4964655381b26b0d94556f55dd5aee6a088116
Javi <8519394+rtomyj@users.noreply.github.com>
2024-03-21 00:02:47 -0500

    Added DB secrets

--- aws-secrets-sync.sh
+++ aws-secrets-sync.sh
@@ -7,7 +7,7 @@ SSL_CERTIFICATE_CRT=$(base64 certs/certificate.crt)
 
 # take current secrets and update the values associated with SSL files - then increment the secret version using today's date
 CERT_FILE_VALUES="{\"SSL_PRIVATE_KEY\": \"$SSL_PRIVATE_KEY\", \"SSL_CA_BUNDLE_CRT\": \"$SSL_CA_BUNDLE_CRT\", \"SSL_CERTIFICATE_CRT\": \"$SSL_CERTIFICATE_CRT\"}"
-UPDATED_SECRETS_STRING=$(aws secretsmanager get-secret-value --secret-id "$SSL_SECRET_MANAGER_ID" --region us-east-2 | jq -r  '.SecretString' | jq  -n ".  + $CERT_FILE_VALUES")
+UPDATED_SECRETS_STRING=$(aws secretsmanager get-secret-value --secret-id "$SSL_SECRET_MANAGER_ID" --region us-east-2 | jq -r  '.SecretString' | jq ".  + $CERT_FILE_VALUES")
 aws secretsmanager put-secret-value --secret-id "$SSL_SECRET_MANAGER_ID" --region us-east-2 --secret-string "$UPDATED_SECRETS_STRING" --no-cli-pager --version-stages "$(date +%F)" "AWSCURRENT"
 
 #############################################
@@ -18,5 +18,15 @@ JWT_PRIVATE_KEY=$(base64 certs/jwt.key)
 JWT_PUBLIC_KEY=$(base64 certs/jwt.pub)
 
 JWT_FILE_VALUES="{\"JWT_PRIVATE_KEY\": \"$JWT_PRIVATE_KEY\", \"JWT_PUBLIC_KEY\": \"$JWT_PUBLIC_KEY\"}"
-JWT_SECRET_STRING=$(echo "" | jq -n ".  + $JWT_FILE_VALUES")
+JWT_SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id "$JWT_SECRET_MANAGER_ID" --region us-east-2 | jq -r  '.SecretString' | jq ".  + $JWT_FILE_VALUES")
 aws secretsmanager put-secret-value --secret-id "$JWT_SECRET_MANAGER_ID" --region us-east-2 --secret-string "$JWT_SECRET_STRING" --no-cli-pager --version-stages "$(date +%F)" "AWSCURRENT"
+
+#############################################
+JWT_SECRET_MANAGER_ID="/prod/project-next/heart-api/db"
+
+# AWS will remove new line characters, which are needed in below files - converting to base64 at the time of saving will resolve this issue
+DB_PEM=$(base64 certs/mongoDB-heart-api-X509.pem)
+
+DB_VALUES="{\"DB_PEM\": \"$DB_PEM\"}"
+DB_SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id "$JWT_SECRET_MANAGER_ID" --region us-east-2 | jq -r  '.SecretString' | jq  ".  + $DB_VALUES")
+aws secretsmanager put-secret-value --secret-id "$JWT_SECRET_MANAGER_ID" --region us-east-2 --secret-string "$DB_SECRET_STRING" --no-cli-pager --version-stages "$(date +%F)" "AWSCURRENT"
