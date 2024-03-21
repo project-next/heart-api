c1825828e8c10d6103adf64e160e0974662099e5
Javi <8519394+rtomyj@users.noreply.github.com>
2024-03-20 21:41:58 -0500

    Added jwt secrets to AWS

--- aws-secrets-sync.sh
+++ aws-secrets-sync.sh
@@ -1,4 +1,4 @@
-SECRET_MANAGER_ID="/prod/project-next/heart-api/ssl"
+SSL_SECRET_MANAGER_ID="/prod/project-next/heart-api/ssl"
 
 # AWS will remove new line characters, which are needed in below files - converting to base64 at the time of saving will resolve this issue
 SSL_PRIVATE_KEY=$(base64 certs/private.key)
@@ -7,5 +7,16 @@ SSL_CERTIFICATE_CRT=$(base64 certs/certificate.crt)
 
 # take current secrets and update the values associated with SSL files - then increment the secret version using today's date
 CERT_FILE_VALUES="{\"SSL_PRIVATE_KEY\": \"$SSL_PRIVATE_KEY\", \"SSL_CA_BUNDLE_CRT\": \"$SSL_CA_BUNDLE_CRT\", \"SSL_CERTIFICATE_CRT\": \"$SSL_CERTIFICATE_CRT\"}"
-UPDATED_SECRETS_STRING=$(aws secretsmanager get-secret-value --secret-id "$SECRET_MANAGER_ID" --region us-east-2 | jq -r  '.SecretString' | jq  ".  + $CERT_FILE_VALUES")
-aws secretsmanager put-secret-value --secret-id "$SECRET_MANAGER_ID" --region us-east-2 --secret-string "$UPDATED_SECRETS_STRING" --no-cli-pager --version-stages "$(date +%F)" "AWSCURRENT"
+UPDATED_SECRETS_STRING=$(aws secretsmanager get-secret-value --secret-id "$SSL_SECRET_MANAGER_ID" --region us-east-2 | jq -r  '.SecretString' | jq  -n ".  + $CERT_FILE_VALUES")
+aws secretsmanager put-secret-value --secret-id "$SSL_SECRET_MANAGER_ID" --region us-east-2 --secret-string "$UPDATED_SECRETS_STRING" --no-cli-pager --version-stages "$(date +%F)" "AWSCURRENT"
+
+#############################################
+JWT_SECRET_MANAGER_ID="/prod/project-next/heart-api/jwt"
+
+# AWS will remove new line characters, which are needed in below files - converting to base64 at the time of saving will resolve this issue
+JWT_PRIVATE_KEY=$(base64 certs/jwt.key)
+JWT_PUBLIC_KEY=$(base64 certs/jwt.pub)
+
+JWT_FILE_VALUES="{\"JWT_PRIVATE_KEY\": \"$JWT_PRIVATE_KEY\", \"JWT_PUBLIC_KEY\": \"$JWT_PUBLIC_KEY\"}"
+JWT_SECRET_STRING=$(echo "" | jq -n ".  + $JWT_FILE_VALUES")
+aws secretsmanager put-secret-value --secret-id "$JWT_SECRET_MANAGER_ID" --region us-east-2 --secret-string "$JWT_SECRET_STRING" --no-cli-pager --version-stages "$(date +%F)" "AWSCURRENT"
